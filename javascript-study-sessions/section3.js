54e0b934bd6bd1d69c468c63ffdf3ec34c631808
Dorian Smith <Smith,h.dorian@gmail.com>
2025-02-12 16:08:17 -0600

    added small daily function

---
+++ javascript-study-sessions/section3.js
@@ -0,0 +1,15 @@
+// Dice Roller
+
+function rollDice(numSides) {
+    return Math.floor(Math.random() * numSides) + 1;
+}
+
+// Uncomment desired dice and run function
+
+// rollDice(4);
+// rollDice(6);
+// rollDice(8);
+// rollDice(10);
+// rollDice(12);
+// rollDice(20);
+// rollDice(100);
